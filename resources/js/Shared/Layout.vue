bc4fc0d4eb4d950385e74dd8e5b0151952cba9b3
Flávio Araújo <flavioafa@gmail.com>
2024-10-20 22:35:00 -0300

    Back ...
    Front
    Melhoria no layout do menu;

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -4,65 +4,61 @@ import { Link } from '@inertiajs/vue3'
 
 <template>
 	<main>
-		<header
-			class="d-flex justify-content-between align-items-center py-3 mb-4 border-bottom"
-		>
-			<nav class="navbar navbar-expand-lg">
-				<div class="container-fluid">
-					<div class="collapse navbar-collapse" id="navbarNav">
-						<ul class="navbar-nav">
-							<li class="nav-item">
-								<Link
-									href="/"
-									class="nav-link"
-									:class="{ active: $page.url === '/' }"
-									>Home
-								</Link>
-							</li>
-							<li class="nav-item">
-								<Link
-									href="/livros"
-									class="nav-link"
-									:class="{ active: $page.url === '/livros' }"
-								>
-									Livros
-								</Link>
-							</li>
-							<li class="nav-item">
-								<Link
-									href="/autores"
-									class="nav-link"
-									:class="{ active: $page.url === '/autores' }"
-								>
-									Autores
-								</Link>
-							</li>
-							<li class="nav-item">
-								<Link
-									href="/assuntos"
-									class="nav-link"
-									:class="{ active: $page.url === '/assuntos' }"
-								>
-									Assuntos
-								</Link>
-							</li>
-							<li class="nav-item">
-								<Link
-									href="/logout"
-									method="post"
-									class="nav-link"
-									as="button"
-									type="button"
-									>Sair
-								</Link>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</nav>
+		<header>
+			<ul class="nav nav-underline justify-content-end">
+				<li class="nav-item">
+					<Link
+						href="/"
+						:class="{ 'nav-link': true, active: $page.url === '/' }"
+						>Home
+					</Link>
+				</li>
+				<li class="nav-item">
+					<Link
+						href="/livros"
+						:class="{ 'nav-link': true, active: $page.url === '/livros' }"
+					>
+						Livros
+					</Link>
+				</li>
+				<li class="nav-item">
+					<Link
+						href="/autores"
+						:class="{ 'nav-link': true, active: $page.url === '/autores' }"
+					>
+						Autores
+					</Link>
+				</li>
+				<li class="nav-item">
+					<Link
+						href="/assuntos"
+						:class="{ 'nav-link': true, active: $page.url === '/assuntos' }"
+					>
+						Assuntos
+					</Link>
+				</li>
+				<li class="nav-item">
+					<Link
+						href="/logout"
+						method="post"
+						class="nav-link"
+						as="button"
+						type="button"
+						>Sair
+					</Link>
+				</li>
+			</ul>
 		</header>
 		<article>
 			<slot />
 		</article>
 	</main>
 </template>
+
+<style>
+.nav-link:hover {
+	color: #0056b3;
+	text-decoration: underline;
+	background-color: #e4f4bc;
+}
+</style>
